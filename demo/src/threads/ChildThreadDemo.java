4424fb001d7d98f237cc42266a6ab8bb4e015319
srikanthpragada <srikanthpragada@gmail.com>
2023-04-29 20:55:54 +0530

    29-APR

--- demo/src/threads/ChildThreadDemo.java
+++ demo/src/threads/ChildThreadDemo.java
@@ -14,14 +14,14 @@ public class ChildThreadDemo {
 	public static void main(String[] args) throws InterruptedException {
         System.out.println("In Main Thread!");
         ChildThread ct = new ChildThread();
-        // ct.setDaemon(true);
+        ct.setDaemon(true);
         ct.start(); // invoking run() 
         
         for (int i = 1; i <= 25; i++)
 			System.out.println("Main : " + i);
         
-        System.out.println("Waiting for child to terminate!");
-        ct.join(); // Main is waiting for child    
+        //System.out.println("Waiting for child to terminate!");
+        //ct.join(); // Main is waiting for child    
         System.out.println("End of Main");
 	}
 
